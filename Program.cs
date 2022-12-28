029e0bb56fe24ac03100d5a8a01ba6ede519bd14
Diana Brik <di.brik@yandex.kz>
2022-12-28 22:01:46 +0600

    Seminar 24.12.22 Task 19

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите пятизначное число: ");
+string? number = Console.ReadLine();
+if (number is not null)
+{
+    if (number.Length == 5)
+    {
+        if ((number[0] == number[4]) && (number[1] == number[3]))
+        {
+            Console.WriteLine($"{number} - палиндром");
+        }
+        else
+        {
+            Console.WriteLine($"{number} - не палиндром");
+        }
+    }
+    else
+    {
+        Console.WriteLine("Введено некорректное значение");
+    }
+}
